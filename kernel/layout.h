741981c1a23274e90668b7b772a5fa8a8ceff352
krinkinmu <krinkin.m.u@gmail.com>
2014-07-31 22:54:36 +0400

    enter protected mode, enable paging

--- kernel/layout.h
+++ kernel/layout.h
@@ -1,8 +1,6 @@
-/* first virtual address of kernel space */
-#define KERNEL_BASE  0xC0000000
+#define VIRTUAL_ADDRESS_BASE 0xC0000000
+#define LINEAR_ADDRESS_BASE  0x00100000
+#define KERNEL_LINK_ADDRESS  (KERNEL_BASE + EXT_MEM_SIZE)
 
-/* actually shift of kernel from KERNEL_BASE */
-#define EXT_MEM_SIZE 0x00100000
-
-/* address where kernel is linked */
-#define KERNEL_LINK  (KERNEL_BASE + EXT_MEM_SIZE)
+#define P2V(x) (x + VIRTUAL_ADDRESS_BASE)
+#define V2P(x) (x - VIRTUAL_ADDRESS_BASE)
---
+++ kernel/mmu.h
@@ -0,0 +1,8 @@
+#define PAGE_SIZE     4096
+#define PD_SIZE       1024
+#define PT_SIZE       1024
+#define PD_ENTRY_SIZE    4
+#define PT_ENTRY_SIZE    4
+
+#define DT_SIZE         16
+#define DT_ENTRY_SIZE    8
